6b67adede11d6ad910b292efdcbdb44b16dcc823
Carlos C <91086892+carloscdno@users.noreply.github.com>
2023-06-29 12:16:21 -0400

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -16,6 +16,7 @@ else
     echo "Welcome, $user! It looks like this is your first time here."
     # insert the customer
     insert_user=$($PSQL "INSERT INTO users(name) VALUES('$user') RETURNING user_id;")
+    # and get their user_id so we can record their game in the games table
     user_id=$($PSQL "SELECT user_id FROM users WHERE name='$user';")
 fi
 
@@ -31,13 +32,16 @@ while true; do
         echo "That is not an integer, guess again:"
         continue
     fi
+    # if the guess is wrong count it so later we can have a final count of the number of guesses
     ((num_guess++))
 
+    # if the guess is correct then let the user know and show them how many tries it took, insert the game and break out of the while loop
     if [[ $guess -eq $secret_number ]]; then
         echo "You guessed it in $num_guess tries. The secret number was $secret_number. Nice job!"
         # Insert the game into the games table
         insert_game=$($PSQL "INSERT INTO games(user_id, num_guess) VALUES($user_id, $num_guess);")
         break
+    # if the guess is wrong let the user know if they have to go higher or lower    
     elif [[ $guess -gt $secret_number ]]; then
         echo "It's lower than that, guess again:"
     else
